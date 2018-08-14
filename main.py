a251864982ff9f3ce3f2c2f966ae490b18085b93
yixuehan <505703394@qq.com>
2018-08-14 17:51:10 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from genErrorCode import genErrors
+
+# ["pro/src/"]不指定则不生成
+# dstDirs = ["../", "/tmp", "."]
+dstDirs = ["."]
+
+# ["c", "java", "go"]不指定则不生成
+# dstLanguages = ["c", "java", "go"]
+dstLanguages = ["go"]
+
+# ["a.err", "b.err"]不指定则默认生成所有的错误码
+srcErrFiles = []
+
+# 需要同步及更新的文件
+gitAddList = ["genErrCode.py", "etc/*.err", "etc/range.config", "etc/*.template", "pkg/*.py", "readme"]
+
+if __name__ == "__main__":
+    genErrors(srcErrFiles, dstDirs, dstLanguages, gitAddList)
+
