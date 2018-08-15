e1df6eb8fa480ca949aee95d340f67328ef2f68e
yixuehan <505703394@qq.com>
2018-08-15 10:35:25 +0800

    更新错误码

--- pkg/JavaGen.py
+++ pkg/JavaGen.py
@@ -8,8 +8,21 @@ class JavaGen:
         self.dstDirs = dstDirs
 
     def gen(self):
+        el = "\n"
+        bl = " " * 4
+        strTemplate = open("etc/java.template", "r").read()
+        # 生成错误码
+        # errs
+        # code    msg    value
+        errList = ""
         for errCode in self.errCodes:
-            pass
+            errList += (bl + "%s(%d, \"%s\")" + el) % (errCode.code, errCode.value, errCode.msg)
+        print errList
+        strTemplate = strTemplate.replace("%ErrList%", errList)
+        # 生成错误映射
+        for dstDir in self.dstDirs:
+            open(dstDir + "/" + "errors.java", "w").write(strTemplate)
+
 
 
 
